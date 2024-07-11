d860ae1701789a9e40139725d7f8157f05d1f60d
Никита Дмитриев <nikita.bmiyrieve@gmail.com>
2024-07-11 21:36:28 +0300

    refreshing file

--- guess_number.py
+++ guess_number.py
@@ -1,14 +1,15 @@
 from random import randint
 
-zag = randint(1,100)
+zag = randint(1, 100)
+
 while True:
     ch_1 = int(input('Введите число: '))
     if ch_1 < zag:
         print('Ваше число меньше того, что загадано')
-        
+
     elif ch_1 > zag:
         print('Ваше число больше того, что загадано')
-        
+
     elif ch_1 == zag:
         print('Отличная интуиция! Вы угадали число :)')
         break
